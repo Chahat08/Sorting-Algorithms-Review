bdc9144cf30b79adb686061da6fc9c7f1ec4ac1a
Chahat Kalsi <chahat.ck88@gmail.com>
2021-10-15 09:15:54 +0530

    comments added

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #define SWAP(x, y, T) do{T t = x; x = y; y = t;} while(0)
 
+/*
+* SELECTION SORT
+*	DISADVANTAGES:
+*		1. Not Stable
+*		2. Runtime O(n^2)
+*		3. The amount of order already present in the list has no effect on runtime
+*		( a sorted or almost sorted list will have about the same runtime as a non sorted one)
+* 
+*	ADVANTAGES:
+*		1. In place (O(1) extra space reqd.)
+*		2. Is the method of choice for sorting items with huge size and small keys
+*		(	The swapping (movement of data) takes place in the outside the inner loop.
+			For items with huge size and small keys, swapping them is of a much higher cost than simple comparison of keys.
+			Selection sort's inner loop is mainly just performing a comparison of keys, so it is preferred in these cases.
+		)
+*/
+
 void SelectionSort(int* arr, int n)
 {
 	if (n == 0 || n == 1) return;
