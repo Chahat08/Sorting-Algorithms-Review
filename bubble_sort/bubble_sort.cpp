7a993ad875dba629d65bd85f295113b18358ed64
Chahat Kalsi <chahat.ck88@gmail.com>
2021-10-15 10:44:36 +0530

    basic bubble sort added

---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void BubbleSort1(int* arr, int n)
+{
+	for (int i = 0; i < n - 1; ++i)
+		for (int j = i + 1; j < n; ++j)
+			if (arr[j] < arr[i]) 
+				std::swap(arr[i], arr[j]);
+}
+void BubbleSort(int* arr, int n)
+{
+	bool swapped;
+	for (int i = 0; i < n - 1; ++i)
+	{
+		swapped = false;
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (arr[j] < arr[i])
+			{
+				std::swap(arr[i], arr[j]);
+				swapped = true;
+			}
+		}
+		if (!swapped) break;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int n = atoi(argv[1]), * arr = new int[n];
+	for (int i = 0; i < n; ++i)std::cin >> arr[i];
+
+	// BubbleSort1(arr, n);
+	BubbleSort(arr, n);
+
+	for (int i = 0; i < n; ++i)std::cout << arr[i] << " ";
+}
